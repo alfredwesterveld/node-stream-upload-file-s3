650b932113392cae0f1772b1a61b520689fde20c
Alfred Westerveld <alfredwesterveld@gmail.com>
2016-08-17 15:05:36 +0200

    Config file and argument parser

--- README.md
+++ README.md
@@ -4,8 +4,34 @@
 $ npm install -g stream-upload-file-s3 
 ```
 
-#using
+#Configure
+
+config.json:
+
+```javascript
+{
+  "accessKeyId": "",
+  "secretAccessKey": "",
+  "region": "eu-west-1",
+  "bucket": ""
+}
+```
+
+#Usage
+
+**command:**
 
 ```bash
-AWS_REGION_FS=<region> AWS_PROFILE=<profile> stream-upload-file-s3 <bucket> <filename> 
+stream-upload-file-s3 -c <config> <file> 
 ```
+
+**Parameters:** 
+
+- _config:_ location of config with json from above
+- _file:_ location of file to put in bucket
+
+**optional:**
+
+- _bucket:_ To override bucket if desired
+
+#Add alias.
--- index.js
+++ index.js
@@ -2,19 +2,39 @@
 
 const fs = require('fs');
 const AWS = require('aws-sdk');
+const argv = require('minimist')(process.argv.slice(2));
+const config = argv.c ? require(argv.c) : require('./config.json');
+
 const args = process.argv;
-const bucket = args[2];
-const filename = args[3];
-const body = fs.createReadStream(filename);
+const filename = args[2];
+const bucket = argv.bucket || config.bucket;
+const region = argv.region || config.region;
+
 const params = {
     Bucket: bucket,
     Key: filename,
     ACL: 'public-read'
 };
 
-AWS.config.region = process.env.AWS_REGION_FS || args[4] || 'eu-west-1';
+if (!filename) {
+    console.log('Usage: <cmd> <filename>');
+    process.exit(1);
+}
 
+if (!config.accessKeyId || !config.secretAccessKey || !bucket) {
+    console.log('please configure ./config.json');
+    process.exit(1);
+}
+
+AWS.config.update({
+    accessKeyId: config.accessKeyId,
+    secretAccessKey: config.secretAccessKey,
+    region: region
+});
+
+const body = fs.createReadStream(filename);
 const s3obj = new AWS.S3({params: params});
+
 s3obj.upload({Body: body}).on('httpUploadProgress', function(evt) {
     console.log(evt);
 }).send(function(err, data) {
