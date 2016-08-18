2a2bc9f8bd198d6d70865f455bf3acd55e55f88d
Alfred Westerveld <alfredwesterveld@gmail.com>
2016-08-18 10:29:24 +0200

    Update CLI

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 const fs = require('fs');
 const AWS = require('aws-sdk');
 const argv = require('minimist')(process.argv.slice(2));
-const config = argv.c ? require(argv.c) : require('./config.json');
-
-const args = process.argv;
-const filename = args[2];
+const config = argv.config ? require(argv.config) : require('./config.json');
+const filename = argv.filename;
 const bucket = argv.bucket || config.bucket;
 const region = argv.region || config.region;
 
@@ -17,7 +15,7 @@ const params = {
 };
 
 if (!filename) {
-    console.log('Usage: <cmd> <filename>');
+    console.log('Usage: stream-upload-file-s3 -config #config -f #filename -bucket #bucket');
     process.exit(1);
 }
 
